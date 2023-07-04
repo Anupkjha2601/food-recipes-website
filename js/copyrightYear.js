61a589f751325c7c1848cfca698c150fd1ae6027
nasirhusain <nasir.swat.hussain@gmail.com>
2023-07-04 04:04:09 -0700

    changes done

--- js/copyrightYear.js
+++
@@ -1,4 +0,0 @@
-let copyRightYear = document.getElementById("copyright-year");
-let currentDate = new Date();
-let currentYear = currentDate.getFullYear();
-copyRightYear.innerText = currentYear;
--- js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.querySelector("body").classList.add("loaded");
-    }, 10)
-  });
